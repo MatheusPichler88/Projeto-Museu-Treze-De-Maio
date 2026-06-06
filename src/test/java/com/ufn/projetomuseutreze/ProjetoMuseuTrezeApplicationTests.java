19b58e5a9a064348d200283048b2a58f31e05f9e
MatheusPichler88 <matheuspichler94@gmail.com>
2026-06-06 17:57:23 -0300

    primeiro commit

---
+++ src/test/java/com/ufn/projetomuseutreze/ProjetoMuseuTrezeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufn.projetomuseutreze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMuseuTrezeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

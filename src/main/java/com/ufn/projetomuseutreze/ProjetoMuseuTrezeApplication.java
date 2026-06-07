16fec7ec8c3231fad85ad9b74e1d1042dfaa42b7
MatheusPichler88 <matheuspichler94@gmail.com>
2026-06-07 00:09:51 -0300

    fix: resolvemos bug de inicialização e adicionamos as variaveis de ambiente.

---
+++ src/main/java/com/ufn/projetomuseutreze/ProjetoMuseuTrezeApplication.java
@@ -0,0 +1,13 @@
+package com.ufn.projetomuseutreze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMuseuTrezeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoMuseuTrezeApplication.class, args);
+    }
+
+}
